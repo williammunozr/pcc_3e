7539de110cb8f1dbb6f3c2835052eb27992cf582
William Munoz <william.munozr@gmail.com>
2023-02-16 23:38:14 -0500

    add sorting lists

---
+++ chapter_03/list_lenght.py
@@ -0,0 +1,7 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+print(f"motorcycles length: {len(motorcycles)}")
+
+# empty list length
+cars = []
+print(f"cars length: {len(cars)}")

0e245b4ba1f1cda2ccfcb38b9d1da3f2fef25e7e
William Munoz <william.munozr@gmail.com>
2023-02-17 17:10:35 -0500

    add exercises to chapter 5

--- chapter_05/ex05/if_test.py
+++ chapter_05/ex05/if_test.py
@@ -5,3 +5,14 @@ for car in cars:
         print(car.upper())
     else:
         print(car.title())
+
+print("")
+
+# validating information with if-elif-else
+age = 13
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $25.")
+else:
+    print("Your admission cost is $40.")

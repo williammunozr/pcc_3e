659f5da3e7a61fba103801c798c83566d6c2d390
William Munoz <william.munozr@gmail.com>
2023-02-16 19:03:43 -0500

    add numbers file

---
+++ chapter_02/numbers.py
@@ -0,0 +1,7 @@
+number1 = 1 # this is an integer
+number2 = 2.0 # this is a float
+number3 = number1 + number2 # operations with floats resulted in a float as well
+
+print(f"number1 value is: {number1} and its type is: {type(number1)}")
+print(f"number2 value is: {number2} and its type is: {type(number2)}")
+print(f"number3 value is: {number3} and its type is: {type(number3)}")

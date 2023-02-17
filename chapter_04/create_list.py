5057647def4d697b25bff1b0138ec118681a0fb5
William Munoz <william.munozr@gmail.com>
2023-02-17 08:03:58 -0500

    working with lists

---
+++ chapter_04/create_list.py
@@ -0,0 +1,4 @@
+captions = [f"caption {value}" for value in range(1, 101)]
+
+for caption in captions:
+    print(caption)
---
+++ chapter_04/list_numbers.py
@@ -0,0 +1,2 @@
+numbers = list(range(1,8))
+print(numbers) # prints [1, 2, 3, 4, 5, 6, 7]

edbe6cedd6358ee2c76c3b7e4b8dfefa533f256e
William Munoz <william.munozr@gmail.com>
2023-02-16 19:19:53 -0500

    add working with numbers

--- chapter_02/numbers.py
+++ chapter_02/numbers.py
@@ -5,3 +5,9 @@ number3 = number1 + number2 # operations with floats resulted in a float as well
 print(f"number1 value is: {number1} and its type is: {type(number1)}")
 print(f"number2 value is: {number2} and its type is: {type(number2)}")
 print(f"number3 value is: {number3} and its type is: {type(number3)}")
+
+number4 = 1.0 + 2.0
+print(f"number4 value is: {number4} and its type is: {type(number4)}")
+
+number5 = 0.1 + 0.3
+print(f"number5 value is: {number5} and its type is: {type(number5)}")

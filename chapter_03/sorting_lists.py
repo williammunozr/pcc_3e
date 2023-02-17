be096270e8143fea19f61340bcad731c76f5b7b4
William Munoz <william.munozr@gmail.com>
2023-02-16 23:25:30 -0500

    add sorting lists

---
+++ chapter_03/sorting_lists.py
@@ -0,0 +1,15 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(f"car list: {cars}")
+
+# sort a list temporary
+print(sorted(cars))
+print(cars)
+
+# sort a list permanently
+cars.sort()
+print(cars)
+
+# sort a list in reverse order
+cars.reverse()
+print(cars)
+

5c938add6984f22e044b95c7d52302f16397385c
William Munoz <william.munozr@gmail.com>
2023-02-18 11:37:34 -0500

    breaking a while

---
+++ chapter_07/ex07/while_break.py
@@ -0,0 +1,10 @@
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\nEnter 'quit' to end the program. "
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print(city.title())

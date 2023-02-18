7135881b4d88d0739468088e5e0b171020273ffb
William Munoz <william.munozr@gmail.com>
2023-02-18 11:09:08 -0500

    working with numeric values in input

---
+++ chapter_07/ex07/input_with_numbers.py
@@ -0,0 +1,7 @@
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 48:
+    print("You are tall enough to ride!")
+else:
+    print("You will be able to ride when you are a little older.")

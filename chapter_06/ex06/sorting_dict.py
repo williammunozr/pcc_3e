400e1fb36e4b7675a36490fa4e642a696a01a05e
William Munoz <william.munozr@gmail.com>
2023-02-18 08:32:30 -0500

    add exercises to chapter 6

--- chapter_06/ex06/sorting_dict.py
+++ chapter_06/ex06/sorting_dict.py
@@ -15,3 +15,10 @@ print("")
 # looping through all the values in a dictionary
 for language in sorted(favorites_languages.values()):
     print(f"{language.title()}")
+
+print("")
+
+# sort a dict in place
+favorites_languages = dict(sorted(favorites_languages.items()))
+for person, language in favorites_languages.items():
+    print(f"Person: {person.title()}, Language: {language.title()}")

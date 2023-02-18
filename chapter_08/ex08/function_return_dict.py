5a09bd7e685f516fe51e66702dc0fa85e2aafb86
William Munoz <william.munozr@gmail.com>
2023-02-18 18:32:20 -0500

    function return dict

--- chapter_08/ex08/function_return_dict.py
+++ chapter_08/ex08/function_return_dict.py
@@ -1,6 +1,8 @@
-def build_person(first_name, last_name):
+def build_person(first_name, last_name, age=None):
     """Return a dictionary of information about a person."""
     person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
     return person
 
 
@@ -9,3 +11,5 @@ print(musician)
 print(musician['first'].title())
 print(musician['last'].title())
 
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)

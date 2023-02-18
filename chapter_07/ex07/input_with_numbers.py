3c915311724028d9264a685dad07e6716576deaf
William Munoz <william.munozr@gmail.com>
2023-02-18 11:14:15 -0500

    working with numeric values in input

--- chapter_07/ex07/input_with_numbers.py
+++ chapter_07/ex07/input_with_numbers.py
@@ -5,3 +5,12 @@ if height >= 48:
     print("You are tall enough to ride!")
 else:
     print("You will be able to ride when you are a little older.")
+
+# modulo
+number = input("Enter a number, and I will tell you if it is even or odd: ")
+number = int(number)
+
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")

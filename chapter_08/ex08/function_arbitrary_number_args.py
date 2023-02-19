3ca6f6cc5b6f80aa211fafcd80630227ed29a6dd
William Munoz <william.munozr@gmail.com>
2023-02-19 11:36:16 -0500

    function with an arbitrary list of parameters

--- chapter_08/ex08/function_arbitrary_number_args.py
+++ chapter_08/ex08/function_arbitrary_number_args.py
@@ -1,6 +1,8 @@
 def make_pizza(*toppings):
-    """Print the list of toppings that have been requested."""
-    print(toppings)
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a pizza whit the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
 
 
 make_pizza('pepperoni')

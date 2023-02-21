a96f0eed35324e7ab8c43facf862e034a5fe86e6
William Munoz <william.munozr@gmail.com>
2023-02-20 23:33:23 -0500

    moving classes to a module

--- chapter_09/ex09/implementing_car.py
+++ chapter_09/ex09/implementing_car.py
@@ -1,4 +1,4 @@
-from car import Car
+from car import Car, ElectricCar
 
 my_new_car = Car('audi', 'a4', 2024)
 print(my_new_car.get_descriptive_name())
@@ -14,3 +14,8 @@ used_car.read_odometer()
 
 used_car.increment_odometer(100)
 used_car.read_odometer()
+
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
+my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
